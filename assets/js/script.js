dcdd6c274228258e67dc1f632b5c5d04ed66e60f
unknown <mubashir.connect@gmail.com>
2025-06-02 19:35:05 +0100

    js functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,39 @@
- 
+document.addEventListener('DOMContentLoaded',function(){
+    let buttons = document.getElementsByTagName('button');
+    for(let button of buttons){
+        button.addEventListener('click', function(){
+            console.log((this.getAttribute('data-type')));
+            if(this.getAttribute('data-type') ==='submit'){
+                
+            }else{
+                let gametype =this.getAttribute('data-type');
+                alert(gametype);
+            }
+        });
+    }
+})
+
+function runGame(){
+
+}
+function checkAnswer(){
+
+}
+function calculateAnswer(){
+
+}
+function incrementScore(){
+
+}
+function incrementWrongAnswer(){
+
+}
+function displayAdditionQuestion(){
+
+}
+function displaysubtractionQuestion(){
+
+}
+function displayMultiplyQuestion(){
+
+}
